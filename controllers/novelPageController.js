d103ed96d62d45608e21ee699a522a2daf02aeef
LeHuuTri-186 <110376741+LeHuuTri-186@users.noreply.github.com>
2024-05-19 10:58:18 +0700

    commit

---
+++ controllers/novelPageController.js
@@ -0,0 +1,52 @@
+const { forever } = require('request-promise');
+const tangthuvien = require('../modules/thichtruyen.js');
+const stringUtil = require('../utils/stringUtil.js')
+
+class NovelPageController {
+    renderNovelPage(req, res) {
+        const novel = req.params.name;
+
+        console.log(req.params.name + " book accessed");
+
+        tangthuvien.crawlAllNovels().then(
+            results => {
+                results.forEach(item => {
+                    console.log(item + " Searched");
+
+                    const itemName = stringUtil.reformatForUrlHandling(item.title);
+
+                    if (novel.localeCompare(itemName) == 0) {
+                    
+                        const cover = item.cover;
+                        const title = item.title;
+                        console.log(item);
+        
+                        let chapColList = "";
+        
+                        tangthuvien.fetchChapterList(item.detailLink).then(
+                            results => {
+                                let count = 1;
+                                results.forEach(element => {
+                                    chapColList += "<li> <a href=" + element + "> Chương " + count + "</a> </li>";
+                                    count++;
+                                })
+        
+                                const renderItems = {
+                                    cover: cover, 
+                                    title: title,
+                                    author: item.chapters,
+                                    chapterList: chapColList
+                                };
+                
+                                res.render('novelPage', renderItems);                            
+                            }
+                        );
+                    }
+                });
+        });
+
+        console.log('Rendering novel page!');
+    }
+};
+
+module.exports = new NovelPageController
