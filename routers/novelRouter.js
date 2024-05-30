b62a18690e93f9b874131eaa54ea5f957e62748c
21120134 <dhthanh1307@gmail.com>
2024-05-30 23:06:44 +0700

    lưu trạng thái và di chuyển nhanh đến trang đang đọc

--- routers/novelRouter.js
+++ routers/novelRouter.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const novelController = require('../controllers/novelPageController.js');
 
 router.use(bodyParser.urlencoded({ extended: true }));
-
+router.use('/', express.json());
 router.route("/:name/page=:page").get(novelController.renderNovelPage);
-
+router.get('/saveStates',novelController.saveStates);
 module.exports = router;
